c7c5a6472bdd4deb4bf4354cc8667c45ec02aff0
himpskatti <sofia.piekkola@hotmail.com>
2017-04-17 01:11:00 +0300

    Edit layout file name after renaming file

--- app/src/main/java/com/sofia/noterecorder/txtBtnFragment.java
+++ app/src/main/java/com/sofia/noterecorder/txtBtnFragment.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * Created by Sofia on 16.4.2017.
@@ -18,7 +17,7 @@ public class txtBtnFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.btn_fragment, container, false);
+        View view = inflater.inflate(R.layout.txt_btn_fragment, container, false);
         return view;
     }
 
