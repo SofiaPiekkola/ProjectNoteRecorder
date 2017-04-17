d51b4dd09220fd8d333c30b4ea41c5ba43b79733
himpskatti <sofia.piekkola@hotmail.com>
2017-04-18 02:26:29 +0300

    Move fragment to directory

--- app/src/main/java/com/sofia/noterecorder/fragments/recordFragment.java
+++ app/src/main/java/com/sofia/noterecorder/fragments/recordFragment.java
@@ -1,4 +1,4 @@
-package com.sofia.noterecorder;
+package com.sofia.noterecorder.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.sofia.noterecorder.R;
 
 /**
  * Created by Sofia on 16.4.2017.
