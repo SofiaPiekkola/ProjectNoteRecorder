551af230d51af6b7ec3afd542990f8c8ce01740b
himpskatti <sofia.piekkola@hotmail.com>
2017-04-18 02:24:02 +0300

    Add listenRecords method that creates new activity for "listen view"

--- app/src/main/java/com/sofia/noterecorder/Activitys/MainActivity.java
+++ app/src/main/java/com/sofia/noterecorder/Activitys/MainActivity.java
@@ -1,16 +1,18 @@
-package com.sofia.noterecorder;
+package com.sofia.noterecorder.Activitys;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.sofia.noterecorder.R;
+import com.sofia.noterecorder.Services.RecordService;
 
 public class MainActivity extends AppCompatActivity {
     String noteState;
@@ -58,5 +60,10 @@ public class MainActivity extends AppCompatActivity {
         Button pressed = (Button) view;
         if (pressed.getText().toString().toLowerCase().contains("note")) pressed.setText("Record sound");
         else pressed.setText("Record note");
+    }
+
+    public void listenRecords(View view) {
+        Intent intent = new Intent(this, ListenRecordsActivity.class);
+        startActivity(intent);
     }
 }
