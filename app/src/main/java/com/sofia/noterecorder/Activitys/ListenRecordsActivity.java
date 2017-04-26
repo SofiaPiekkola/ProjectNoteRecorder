1c74ba03ea0e6962ff228302c3b98b8fcd2dac79
himpskatti <sofia.piekkola@hotmail.com>
2017-04-26 22:25:30 +0300

    Edit background color when file is selected

--- app/src/main/java/com/sofia/noterecorder/Activitys/ListenRecordsActivity.java
+++ app/src/main/java/com/sofia/noterecorder/Activitys/ListenRecordsActivity.java
@@ -1,6 +1,7 @@
 package com.sofia.noterecorder.Activitys;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.sofia.noterecorder.R;
@@ -17,8 +18,9 @@ public class ListenRecordsActivity extends AppCompatActivity {
 
     public void soundSelected(SoundFile soundFile) {
         PlayFragment playFragment = (PlayFragment) getFragmentManager().findFragmentById(R.id.playFileFragment);
-        if (playFragment != null && playFragment.isVisible()){
+        if ( playFragment!= null && playFragment.isVisible()){
             playFragment.initialiseButtons(soundFile.getPath(), soundFile.getName());
+            playFragment.getView().setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary));
         }else {
             Intent intent = new Intent(this, PlayActivity.class);
             intent.putExtra("name", soundFile.getName());
