217628ab0127a6f4b6336a0803fa7d99898d9f3c
himpskatti <sofia.piekkola@hotmail.com>
2017-05-03 16:28:08 +0300

    Move menus to own activity to display in all activities

--- app/src/main/java/com/sofia/noterecorder/Activitys/MainActivity.java
+++ app/src/main/java/com/sofia/noterecorder/Activitys/MainActivity.java
@@ -7,9 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,7 +14,7 @@ import android.widget.Toast;
 import com.sofia.noterecorder.R;
 import com.sofia.noterecorder.Services.RecordService;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends MenuActivity {
     String noteState;
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private boolean permissionToRecordAccepted = false;
@@ -28,13 +25,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.mainmenu, menu);
-        return true;
     }
 
     @Override
@@ -75,14 +65,5 @@ public class MainActivity extends AppCompatActivity {
     public void listenRecords(View view) {
         Intent intent = new Intent(this, ListenRecordsActivity.class);
         startActivity(intent);
-    }
-
-    public void sendFeedBack(MenuItem item) {
-        Intent Email = new Intent(Intent.ACTION_SEND);
-        Email.setType("text/email");
-        Email.putExtra(Intent.EXTRA_EMAIL, new String[] { "noterecorder@outlook.com" });
-        Email.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
-        Email.putExtra(Intent.EXTRA_TEXT, "Please add your feedback here");
-        startActivity(Intent.createChooser(Email, "Select your e-mail service:"));
     }
 }
