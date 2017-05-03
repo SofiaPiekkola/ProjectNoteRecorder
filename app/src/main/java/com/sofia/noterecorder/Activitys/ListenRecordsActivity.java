5c6c3d287beb613609991d6a17f31e8042befa3a
himpskatti <sofia.piekkola@hotmail.com>
2017-05-03 16:27:09 +0300

    Edit file to extend MenuActivity to display menu

--- app/src/main/java/com/sofia/noterecorder/Activitys/ListenRecordsActivity.java
+++ app/src/main/java/com/sofia/noterecorder/Activitys/ListenRecordsActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.sofia.noterecorder.R;
 import com.sofia.noterecorder.Resources.SoundFile;
 import com.sofia.noterecorder.fragments.PlayFragment;
 
-public class ListenRecordsActivity extends AppCompatActivity {
+public class ListenRecordsActivity extends MenuActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
