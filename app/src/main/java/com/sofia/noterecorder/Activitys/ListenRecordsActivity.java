644b48ccff1050be8724c8e3853bf11344d0c2d2
himpskatti <sofia.piekkola@hotmail.com>
2017-05-08 03:51:28 +0300

    Add possibility to see file duration

--- app/src/main/java/com/sofia/noterecorder/Activitys/ListenRecordsActivity.java
+++ app/src/main/java/com/sofia/noterecorder/Activitys/ListenRecordsActivity.java
@@ -1,12 +1,16 @@
 package com.sofia.noterecorder.Activitys;
 
 import android.content.Intent;
+import android.media.MediaMetadataRetriever;
 import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
 
 import com.sofia.noterecorder.R;
 import com.sofia.noterecorder.Resources.SoundFile;
 import com.sofia.noterecorder.fragments.PlayFragment;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class ListenRecordsActivity extends BaseActivity {
 
@@ -18,14 +22,25 @@ public class ListenRecordsActivity extends BaseActivity {
 
     public void soundSelected(SoundFile soundFile) {
         PlayFragment playFragment = (PlayFragment) getFragmentManager().findFragmentById(R.id.playFileFragment);
+        int duration = getFileDuration(soundFile.getPath());
         if ( playFragment!= null && playFragment.isVisible()){
-            playFragment.initialiseButtons(soundFile.getPath(), soundFile.getName());
+            playFragment.initialiseButtons(soundFile.getPath(), soundFile.getName(), duration);
             playFragment.getView().setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary));
         }else {
             Intent intent = new Intent(this, PlayActivity.class);
             intent.putExtra("name", soundFile.getName());
             intent.putExtra("path", soundFile.getPath());
+            intent.putExtra("duration", duration);
             startActivity(intent);
         }
+    }
+
+    public int getFileDuration(String path) {
+        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+        mmr.setDataSource(path);
+        String duration = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        int seconds = (Integer.parseInt(duration) / 1000);
+        mmr.release();
+        return seconds;
     }
 }
