888639a20c11ccf608fa4a48e0cc08aee690d27d
himpskatti <sofia.piekkola@hotmail.com>
2017-05-08 04:38:39 +0300

    Save selected settings

--- app/src/main/java/com/sofia/noterecorder/Activitys/Settings.java
+++ app/src/main/java/com/sofia/noterecorder/Activitys/Settings.java
@@ -1,19 +1,37 @@
 package com.sofia.noterecorder.Activitys;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.sofia.noterecorder.R;
 
 public class Settings extends BaseActivity {
-    static int language = 1;
+    public static int language = 1;
     public static int recordType = 1;
-
+    SharedPreferences.Editor editor;
+    SharedPreferences sharedPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
+        getValues();
+    }
+
+    void getValues(){
+        RadioButton rb = (RadioButton)findViewById(R.id.langEnglish);
+        rb.setChecked(sharedPref.getBoolean("langEnglish", true));
+        rb = (RadioButton)findViewById(R.id.langFinnish);
+        rb.setChecked(sharedPref.getBoolean("langFinnish", false));
+
+        rb = (RadioButton)findViewById(R.id.typeTHREE_GPP);
+        rb.setChecked(sharedPref.getBoolean("typeTHREE_GPP", true));
+        rb = (RadioButton)findViewById(R.id.typeMPEG_4);
+        rb.setChecked(sharedPref.getBoolean("typeMPEG_4", false));
     }
 
     public void saveSettings(View view) {
@@ -31,6 +49,22 @@ public class Settings extends BaseActivity {
 
         System.out.println(language + ", " + recordType);
 
+        saveSelected();
         finish();
+    }
+
+    void saveSelected(){
+        editor = sharedPref.edit();
+        RadioButton rb = (RadioButton)findViewById(R.id.langEnglish);
+        editor.putBoolean("langEnglish", rb.isChecked());
+        rb = (RadioButton)findViewById(R.id.langFinnish);
+        editor.putBoolean("langFinnish", rb.isChecked());
+
+        rb = (RadioButton)findViewById(R.id.typeTHREE_GPP);
+        editor.putBoolean("typeTHREE_GPP", rb.isChecked());
+        rb = (RadioButton)findViewById(R.id.typeMPEG_4);
+        editor.putBoolean("typeMPEG_4", rb.isChecked());
+
+        editor.apply();
     }
 }
