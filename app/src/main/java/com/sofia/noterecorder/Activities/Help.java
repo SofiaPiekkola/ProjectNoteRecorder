8440e9c2a5b29355c311874035d01104190ae04f
himpskatti <sofia.piekkola@hotmail.com>
2017-05-10 18:35:34 +0300

    Added JavaDoc

--- app/src/main/java/com/sofia/noterecorder/Activities/Help.java
+++ app/src/main/java/com/sofia/noterecorder/Activities/Help.java
@@ -3,7 +3,21 @@ package com.sofia.noterecorder.Activities;
 import android.os.Bundle;
 import com.sofia.noterecorder.R;
 
+/**
+ * @author Sofia Piekkola
+ * @version 1.0
+ * @since 10.5.2017
+ *
+ *
+ * Help displays the help.
+ */
 public class Help extends BaseActivity {
+
+    /**
+     * Creates help view
+     *
+     * @param savedInstanceState - mapping from String keys
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
