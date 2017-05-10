093056c950ff74f10f46802fa01f8ac96c84c588
himpskatti <sofia.piekkola@hotmail.com>
2017-05-10 20:22:27 +0300

    Added JavaDoc

--- app/src/main/java/com/sofia/noterecorder/fragments/RecordFragment.java
+++ app/src/main/java/com/sofia/noterecorder/fragments/RecordFragment.java
@@ -6,10 +6,26 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.sofia.noterecorder.R;
 
+/**
+ * @author Sofia Piekkola
+ * @version 1.0
+ * @since 10.5.2017
+ *
+ *
+ * RecordFragment displays the record button in the first view.
+ */
 public class RecordFragment extends Fragment {
+
+    /**
+     * Instantiates the view
+     *
+     * @param inflater - used to inflate any views in the fragment
+     * @param container - parent view that the fragment's UI is attached to
+     * @param savedInstanceState - a previous saved state
+     * @return - view for the fragment's UI
+     */
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
