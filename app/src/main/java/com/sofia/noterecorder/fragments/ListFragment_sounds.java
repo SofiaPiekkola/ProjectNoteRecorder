aa17c0f245f72b5ff87d50aa76f7d0f783475329
himpskatti <sofia.piekkola@hotmail.com>
2017-05-10 19:58:27 +0300

    Added JavaDoc

--- app/src/main/java/com/sofia/noterecorder/fragments/ListFragment_sounds.java
+++ app/src/main/java/com/sofia/noterecorder/fragments/ListFragment_sounds.java
@@ -18,27 +18,61 @@ import com.sofia.noterecorder.Resources.SoundFile;
 import java.io.File;
 import java.util.ArrayList;
 
+/**
+ * @author Sofia Piekkola
+ * @version 1.0
+ * @since 10.5.2017
+ *
+ *
+ * ListFragment_sounds displays the list of sounds used in the second tab of portrait orientation
+ */
 public class ListFragment_sounds extends Fragment {
+
+    /**
+     * List of all the notes in directory.
+     */
     private final ArrayList<SoundFile> sounds = new ArrayList<>();
+
+    /**
+     * One row of the list
+     */
     private View row;
 
+    /**
+     * Creates the list view
+     *
+     * @param savedInstanceState - mapping from String keys
+     */
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        createSounds("/sounds/open", sounds);
-        createSounds("/sounds/close", sounds);
+        createSounds("/sounds/open");
+        createSounds("/sounds/close");
     }
 
-    private void createSounds(String file, ArrayList<SoundFile> list) {
-        @SuppressWarnings("ConstantConditions")
-        String path = getActivity().getExternalCacheDir().getAbsolutePath() + file;
+    /**
+     * Creates sounds to be displayed in the list
+     *
+     * @param path - end part of the path that contains sounds
+     */
+    @SuppressWarnings("ConstantConditions")
+    private void createSounds(String path) {
+        path = getActivity().getExternalCacheDir().getAbsolutePath() + path;
         File dir = new File(path);
         File[] files = dir.listFiles();
         if (files != null) {
-            for (File f : files) list.add(new SoundFile(f.getName(), path));
+            for (File f : files) sounds.add(new SoundFile(f.getName(), path));
         }
     }
 
+    /**
+     * Instantiates the view
+     *
+     * @param inflater - used to inflate any views in the fragment
+     * @param container - parent view that the fragment's UI is attached to
+     * @param savedInstanceState - a previous saved state
+     * @return - view for the fragment's UI
+     */
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -48,19 +82,14 @@ public class ListFragment_sounds extends Fragment {
         ArrayAdapter<SoundFile> soundAdapter = new ArrayAdapter<>(getActivity(), R.layout.sound_file,  R.id.text, sounds);
         listView.setAdapter(soundAdapter);
         addClickListener(listView);
-        selectFirstIfInLandscape(listView);
         return view;
     }
 
-    private void selectFirstIfInLandscape(ListView listView) {
-        if (isAdded()) {
-            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                new Handler().post(() -> listView.performItemClick(listView.getChildAt(0), 0,
-                        listView.getAdapter().getItemId(0)));
-            }
-        }
-    }
-
+    /**
+     * Add click listener for all the sounds in the list
+     *
+     * @param listView - list that contains sound items
+     */
     private void addClickListener(ListView listView) {
         listView.setOnItemClickListener((parent, view, position, id) -> {
             if (view != null) {
@@ -69,7 +98,6 @@ public class ListFragment_sounds extends Fragment {
                 view.setBackgroundResource(R.color.colorPrimary);
                 ((ListenRecordsActivity) getActivity()).soundSelected(sounds.get(position));
             }
-            else selectFirstIfInLandscape(listView);
         });
     }
 }
